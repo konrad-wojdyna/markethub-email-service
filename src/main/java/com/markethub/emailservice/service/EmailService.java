89718ca8310583e29268ebf2006e667acb04593b
konrad-wojdyna <konrad.wojdyna@gmail.com>
2026-05-01 15:18:18 +0200

    feat: add EmailService with SimpleMailMessage

--- src/main/java/com/markethub/emailservice/service/EmailService.java
+++ src/main/java/com/markethub/emailservice/service/EmailService.java
@@ -3,17 +3,25 @@ package com.markethub.emailservice.service;
 
 import com.markethub.emailservice.model.ListingCreatedEmailRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    private RabbitTemplate rabbitTemplate;
+    private final JavaMailSender mailSender;
 
     public void sendListingCreatedEmail(ListingCreatedEmailRequest request){
 
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(request.userEmail());
+        message.setSubject("Twoje ogłoszenie zostało opublikowane: " + request.title());
+        message.setText("Cześć " + request.userFirstName() + ", Twoje ogłoszenie " +
+                request.title() + " zostało opublikowane.");
+
+         mailSender.send(message);
     }
 
 }
