b299a92163960a1591047309dcb08f0fbf4c681e
konrad-wojdyna <konrad.wojdyna@gmail.com>
2026-05-01 15:10:40 +0200

    feat: add Spring Mail configuration with MailHog

--- src/main/java/com/markethub/emailservice/service/EmailService.java
+++ src/main/java/com/markethub/emailservice/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.markethub.emailservice.service;
 
 
+import com.markethub.emailservice.model.ListingCreatedEmailRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,8 @@ public class EmailService {
 
     private RabbitTemplate rabbitTemplate;
 
+    public void sendListingCreatedEmail(ListingCreatedEmailRequest request){
+
+    }
 
 }
